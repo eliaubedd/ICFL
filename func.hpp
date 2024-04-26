15531cd79ab9262ed16521dcd3bbf50905cf9c5e
eliamartin <elialeonardo.martin@gmail.com>
2024-04-26 15:50:28 +0200

    Terminata getInsertionTarget

--- func.hpp
+++ func.hpp
@@ -1,7 +1,160 @@
+/**
+ * @file funch.cpp
+ * @brief Contiene le funzioni di supporto per BitVector e List, .
+ */
+
 #include <pasta/bit_vector/bit_vector.hpp>
 #include <pasta/bit_vector/support/flat_rank_select.hpp>
 #include <iostream>
 #include <list>
 #include <string>
 
+/**
+ * @brief Costruisce e restituisce una lista di stringhe di esempio.
+ *
+ * Costruisce una lista di stringhe di esempio e la restituisce.
+ *
+ * @return Lista di stringhe.
+ */
+std::list<std::string> build_ICFL(){
+    std::list<std::string> icfl_text;
+    icfl_text.push_back("aaa");
+    icfl_text.push_back("baa");
+    icfl_text.push_back("caabca");
+    icfl_text.push_back("dcaabca");
+    return icfl_text;
+}
 
+/**
+ * @brief Costruisce e restituisce una [x]_g-list di esempio.
+ *
+ * Costruisce una lista di interi di esempio e la restituisce.
+ *
+ * @return Lista di interi.
+ */
+std::list<int> build_xgList(){
+    std::list<int> xglist;
+    xglist.push_back(19);
+    xglist.push_back(3);
+    xglist.push_back(6);
+    xglist.push_back(12);
+    return xglist;
+}
+
+/**
+ * @brief Costruisce e restituisce una [z]_g-list di esempio.
+ *
+ * Costruisce una lista di interi di esempio e la restituisce.
+ *
+ * @return Lista di interi.
+ */
+std::list<int> build_zgList(){
+    std::list<int> zglist;
+    zglist.push_back(16);
+    zglist.push_back(9);
+    return zglist;
+}
+
+/**
+ * @brief Stampa gli elementi di una lista generica.
+ *
+ * Stampa gli elementi contenuti in una lista generica.
+ *
+ * @tparam T Tipo degli elementi contenuti nella lista.
+ * @param list Lista da stampare.
+ */
+template <typename T>
+void print_list(std::list<T> &list){
+    std::cout << " < ";
+    for (const T& x : list) {
+        std::cout << x << " ";
+    }
+    std::cout << "> " << std::endl;
+}
+
+/**
+ * @brief Stampa i bit presenti in un oggetto BitVector.
+ *
+ * Stampa i bit presenti in un oggetto BitVector.
+ *
+ * @param bv Oggetto BitVector da stampare.
+ */
+void print_bv(pasta::BitVector &bv){
+    for (auto &it : bv){
+        std::cout << (it ? '1' : '0');
+    }
+    std::cout << std::endl;
+}
+
+/**
+ * @brief Trova l'elemento all'indice specificato in una lista.
+ *
+ * Questa funzione cerca l'elemento all'indice specificato nella lista data e restituisce un iteratore
+ * alla sua posizione se trovato. Se l'indice specificato è fuori dall'intervallo della lista,
+ * viene lanciata un'eccezione std::out_of_range.
+ *
+ * @tparam T Il tipo degli elementi memorizzati nella lista.
+ * @param list La lista in cui effettuare la ricerca.
+ * @param i L'indice dell'elemento da trovare.
+ * @return Un iteratore alla posizione dell'elemento all'indice specificato se trovato.
+ * @throw std::out_of_range se l'indice specificato è fuori dall'intervallo della lista.
+ */
+template <typename T>
+typename std::list<T>::iterator find_node(std::list<T> &list, unsigned int i){
+    unsigned int count = 0;
+    auto it = list.begin();
+    while (it != list.end()) {
+        if (count == i){
+            return it;
+        }
+        ++it;
+        ++count;
+    }
+    throw std::out_of_range("Indice fuori dalla lista");
+}
+
+/**
+ * @brief Calcola e restituisce il punto di inserimento "insertion target" in base ai parametri forniti.
+ *
+ * Calcola e restituisce il punto di inserimento "insertion target" in base ai parametri forniti.
+ *
+ * @param b_x BitVector associato al suffiso x.
+ * @param b_z BitVector associato al suffisso z = xy.
+ * @param icfl_t Lista di stringhe ICFL.
+ * @param y Suffisso di x.
+ * @return Punto di inserimento calcolato.
+ */
+unsigned int getInsertionTarget(pasta::BitVector& b_x, pasta::BitVector& b_z, std::list<std::string> &icfl_t, std::string y){
+    unsigned int i = 0, k = 0, u = 0, p = 0, s = 0, q = 0, h = 0;
+    std::string alpha;
+
+    pasta::FlatRankSelect rs_x(b_x);
+    pasta::FlatRankSelect rs_z(b_z);
+
+    i = rs_z.select1(1);
+    k = b_x.size();
+    s = b_x[k] + 1;
+
+    if(i < k){
+        u = rs_z.rank1(i-1) + b_x[k] + 1;
+    }
+    else{
+        u = rs_z.rank1(b_z.size()) + 1;
+    }
+
+    p = u - 1;
+
+    while(p >= s){
+        q = rs_x.select1(p - b_x[k]);
+        alpha = (*find_node(icfl_t, q+1)).substr(0,y.size());
+        if(alpha <= y){
+            p = s - 1;
+        }
+        else{
+            p = p - 1;
+        }
+    }
+
+    h = p + 1;
+    return h;
+}
--- main.cpp
+++ main.cpp
@@ -1,89 +1,36 @@
 #include "func.hpp"
 
-std::list<std::string> build_ICFL(void){
-  std::list<std::string> icfl_text;
-  icfl_text.push_back("aaa");
-  icfl_text.push_back("baa");
-  icfl_text.push_back("caabca");
-  icfl_text.push_back("dcaabca");
-  return icfl_text;
-}
-
-void print_list(std::list<std::string> &list){
-  for (std::string& x : list) {
-        std::cout << x << " ";
-  }
-}
-
-void print_bv(pasta::BitVector &bv){
-  for (auto it = bv.begin(); it != bv.end(); ++it){
-    std::cout << ((*it == true) ? '1' : '0');
-  }
-  std::cout << std::endl;
-}
-
-int getInsertionTarget(pasta::BitVector& b_x, pasta::BitVector& b_z, const std::list<std::string> icfl_t){
-  int i = 0, k = 0, u = 0, p = 0, s = 0, q = 0;
-  int y = 3;
-  std::string alpha = "";
-
-  pasta::FlatRankSelect rs_x(b_x);
-  pasta::FlatRankSelect rs_z(b_z);
-  
-  i = rs_x.select1(1);
-  k = b_x.size();
-  s = b_x[k] + 1;
-  //calcolo upperbound
-  if(i < k){
-    u = rs_z.rank1(i-1) + b_x[k] + 1;
-  }
-  else{
-    u = rs_z.rank1(b_z.size()) + 1; 
-  }
-
-  p = u - 1;
-
-  //TODO
-  /*
-  while(p >= s){
-    q = rs_x.select1(p - b_x[k]);
-
-    for(int c = 0; c < icfl_t.size(); c++){
-
-    }
-  }
-  */
-  
-
-
-}
-
 int main()
 {
-      // Create a bit vector of size 1000 containing only zeros and flip every other bit.
-  pasta::BitVector bv_x(4, 1);
-  pasta::BitVector bv_z(4,0);
-  bv_z[0] = 1; 
-  bv_z[1] = 1;
-  print_bv(bv_x);
-  print_bv(bv_z);
+    //Create text
+    std::list<std::string> icfl_text = build_ICFL();
+    std::cout << "Sia ICFL(T) =" ; print_list(icfl_text);
 
+    //Create suffix
+    std::string x = "a";
+    std::string z = "abca";
+    std::string y = "bca";
+    std::cout << "Siano x = " << x << " e z = xy = " << z << std::endl;
 
-  /*
-    // Create rank and select support and print the result of some queries.
-  pasta::FlatRankSelect rs(bv);
-  std::cout << rs.rank0(250) << ", " << rs.rank1(250)
-            << ", "
-            << rs.select0(250) << ", " << rs.rank1(250)
-            << std::endl;
-  */
+    //Create x and z list
+    std::list<int> xgList = build_xgList();
+    std::list<int> zgList = build_zgList();
+    std::cout << "[x]_g-list = "; print_list(xgList);
+    std::cout << "[z]_g-list = "; print_list(zgList);
 
-  std::list<std::string> icfl_text = build_ICFL();
-  print_list(icfl_text);
-  std::string x = "a";
-  std::string z = "abca";
-  std::cout << getInsertionTarget(bv_x, bv_x, icfl_text);
+    //Create a bit vectors
+    pasta::BitVector bv_x(4, 1);
+    pasta::BitVector bv_z(4,0);
+    bv_z[2] = 1;
+    bv_z[3] = 1;
+    std::cout << "bit vector Bx: "; print_bv(bv_x);
+    std::cout << "bit vector Bz: "; print_bv(bv_z);
 
-  return 0;
+    std::cout << *find_node(icfl_text, 3) << std::endl;
+    unsigned int h = getInsertionTarget(bv_x, bv_z, icfl_text, y);
+    std::cout << "Insertion target h: " << h << std::endl;
+    std::cout << "Nodo dell'insertion target: " << *find_node(xgList, h) << std::endl;
+
+    return 0;
 
 }
